b4be98332b69836156d214b90f75e6e6fc4f986f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-19 12:06:53 +0100

    Header file

--- monty.h
+++ monty.h
@@ -1,5 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -15,6 +16,7 @@ typedef struct stack_s
 	struct stack_s *prev;
 	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -28,3 +30,5 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+#endif /* MONTY_H */
