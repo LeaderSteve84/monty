9bf2b34f3db7facdc044f3ec5df0fc48f3b90378
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-22 20:43:25 +0100

    div function

--- 102-more_stack.c
+++ 102-more_stack.c
@@ -119,7 +119,7 @@ void sub(stack_t **head, unsigned int line_number)
 
 	if (*my_stack == NULL || my_stack == NULL)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free_arguments_node();
 		exit(EXIT_FAILURE);
 	}
@@ -130,7 +130,7 @@ void sub(stack_t **head, unsigned int line_number)
 	}
 	if (node_count < 2)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free_arguments_node();
 		exit(EXIT_FAILURE);
 	}
@@ -140,6 +140,54 @@ void sub(stack_t **head, unsigned int line_number)
 		{
 			diff = current->next->n - current->n;
 			current->next->n = diff;
+			*head = current->next;
+			current->next->prev = NULL;
+			free(current);
+		}
+	}
+}
+
+/**
+ * div - Divides the first two data on the stack and pops
+ * @head: The whole stack
+ * @line_number: The current line
+ * Return: Nothing
+ */
+
+void div(stack_t **head, unsigned int line_number)
+{
+	int node_count = 0, result;
+	stack_t *temp = *head, *current = *head;
+
+	if (*my_stack == NULL || my_stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		free_arguments_node();
+		exit(EXIT_FAILURE);
+	}
+	while (temp != NULL)
+	{
+		node_count++;
+		temp = temp->next;
+	}
+	if (node_count < 2)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		free_arguments_node();
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (current != NULL && current->next != NULL)
+		{
+			if (current->n == 0)
+			{
+				fprintf(stderr, "L%u: division by zero\n", line_number);
+				free_arguments_node();
+				exit(EXIT_FAILURE);
+			}
+			result = current->next->n / current->n;
+			current->next->n = result;
 			*head = current->next;
 			current->next->prev = NULL;
 			free(current);
