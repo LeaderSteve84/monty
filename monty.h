0aaf2bf9ed09ca5f09e5bd75cbeaf94faf90754f
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-23 19:35:51 +0100

    Header

--- monty.h
+++ monty.h
@@ -89,6 +89,7 @@ void divide(stack_t **head, unsigned int line_number);
 void mod(stack_t **head, unsigned int line_number);
 void pchar(stack_t **head, unsigned int line_number);
 void pstr(stack_t **head, unsigned int line_number);
+void rotl(stack_t **head, unsigned int line_number);
 int _putchar(char c);
 void mul(stack_t **head, unsigned int line_number);
 int check_string(char *string);
