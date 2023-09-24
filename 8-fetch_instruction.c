cf644cf6121e437054fb5597443d2bcc761292b2
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2023-09-24 21:54:21 +0100

    comments

--- 8-fetch_instruction.c
+++ 8-fetch_instruction.c
@@ -19,6 +19,8 @@ void fetch_instruction(void)
 	{
 		return;
 	}
+	if (arguments->toks_arr[0] && arguments->toks_arr[0] == "#")
+		return;
 	for (i = 0; instructions[i].opcode != NULL; i++)
 	{
 		if (strcmp(instructions[i].opcode, arguments->toks_arr[0]) == 0)
